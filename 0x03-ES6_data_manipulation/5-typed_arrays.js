e5bfdd6c9ff3f418e87b69a322af2baa9f776691
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2024-04-17 01:11:02 +0200

    taskFive

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(
   position,
   value,
 ) {
-  if (position >= length) {
+  if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
 
