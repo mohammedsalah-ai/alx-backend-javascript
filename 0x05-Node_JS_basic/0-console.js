a17b7a2b52dfc4cab61007a601508defc335f4c2
Mohammed Salah <mohammedsalah.superm159@gmail.com>
2024-07-24 09:40:05 +0300

    taskZero

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
 function displayMessage(msg) {
-  console.log(msg)
+  console.log(msg);
 }
+
+module.exports = displayMessage;
